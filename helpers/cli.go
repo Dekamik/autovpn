57fc4d86b39c48988909a72f7008b937218d3402
Dennis von Bargen <dennis@vonbargen.se>
2022-09-29 20:53:48 +0200

    Removed sudo-related workarounds

--- helpers/cli.go
+++ helpers/cli.go
@@ -1,8 +1,8 @@
 package helpers
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 // WaitPrint is a goroutine that prints the message and prints dots while waiting.
@@ -10,28 +10,25 @@ import (
 // To await method completion, simply call <-exited.
 // This command uses VT100 escape codes to erase the line in terminal.
 func WaitPrint(message string, finish chan bool, exited chan bool) {
-	startTime := time.Now().Local()
-	wheel := "-\\|/"
-	i := 0
-	eraseCode := map[string]string{
-		"bash":    "\u001B[2K\r",
-		"windows": "ESC[K",
-	}
-	for {
-		select {
-		case <-finish:
-			fmt.Printf("%s%-35s %-6s OK\n", eraseCode["bash"], message, time.Since(startTime).Truncate(time.Second))
-			exited <- true
-			return
-		default:
-			fmt.Printf("%s%-35s %-6s %s ", eraseCode["bash"], message, time.Since(startTime).Truncate(time.Second), string(wheel[i]))
-			time.Sleep(time.Millisecond * 100)
+    startTime := time.Now().Local()
+    wheel := "-\\|/"
+    i := 0
+    eraseCode := "\u001B[2K\r"
+    for {
+        select {
+        case <-finish:
+            fmt.Printf("%s%-35s %-6s OK\n", eraseCode, message, time.Since(startTime).Truncate(time.Second))
+            exited <- true
+            return
+        default:
+            fmt.Printf("%s%-35s %-6s %s ", eraseCode, message, time.Since(startTime).Truncate(time.Second), string(wheel[i]))
+            time.Sleep(time.Millisecond * 100)
 
-			if i == len(wheel)-1 {
-				i = 0
-			} else {
-				i++
-			}
-		}
-	}
+            if i == len(wheel)-1 {
+                i = 0
+            } else {
+                i++
+            }
+        }
+    }
 }
