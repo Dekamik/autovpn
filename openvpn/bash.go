33366dde8314ffd8f53c72f62dbb1c0ca00130a3
Dennis von Bargen <dennis.von-bargen@viati.se>
2022-09-29 17:39:14 +0200

    Removed obsolete sudo prompt from bash

--- openvpn/bash.go
+++ openvpn/bash.go
@@ -5,6 +5,6 @@ package openvpn
 import "os/exec"
 
 func ovpnConnect(ovpnConfig string) *exec.Cmd {
-	return exec.Command("sudo", "openvpn", "--config", ovpnConfig, "--verb", "0", "--script-security", "2",
+	return exec.Command("openvpn", "--config", ovpnConfig, "--verb", "0", "--script-security", "2",
 		"--up", "/bin/echo \"Connected! Press CTRL+C to disconnect\n\"")
 }
