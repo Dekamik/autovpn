51a079a30517cafdc9644b2e278a80f59336cd8b
Dennis von Bargen <dennis@vonbargen.se>
2022-10-01 16:30:34 +0200

    Added override for executables in config file

--- openvpn/windows.go
+++ openvpn/windows.go
@@ -7,7 +7,24 @@ import (
     "os/exec"
 )
 
-func ovpnConnect(ovpnConfig string) *exec.Cmd {
-    return exec.Command("Powershell", "Start", "C:\\Program` Files\\OpenVPN\\bin\\openvpn.exe",
-        "-ArgumentList", fmt.Sprintf("--config,%s,--verb,0", ovpnConfig), "-NoNewWindow")
+func IsInstalled(executable string) bool {
+    // If the version command returns an ExitError, we know it's installed.
+    // If it's not installed, it returns a regular Error type.
+    cmd := exec.Command("Powershell", "Start", executable, "-ArgumentList", "--version", "-NoNewWindow")
+    err := cmd.Run()
+    target := &exec.ExitError{}
+    return errors.As(err, &target)
+}
+
+func GetExecutable(executableOverride string) string {
+    if len(executableOverride) == 0 {
+        return "C:\\Program` Files\\OpenVPN\\bin\\openvpn.exe"
+    } else {
+        return executableOverride
+    }
+}
+
+func ovpnConnect(executable string, ovpnConfig string) *exec.Cmd {
+    return exec.Command("Powershell", "Start", executable, "-ArgumentList",
+        fmt.Sprintf("--config,%s,--verb,0", ovpnConfig), "-NoNewWindow")
 }
